7331842a25c41e10ad312b3e6b2f1331356044f5
pc <pikus@list.pl>
2020-02-13 03:27:38 +0100

    Ustawienie projektu pod demo.

--- bin/example_4x4_3us.ml
+++ bin/example_4x4_3us.ml
@@ -158,6 +158,28 @@ let estConn1AF_rhs_to_parse =
 ({}, {t1}, {(0, 1)})
 "
 
+let breConn_lhs_to_parse = 
+"
+{(0, UAV:1),(1, UAV:1)}
+2 2 0
+10
+01
+00
+00
+({}, {c1}, {(0, 1), (1, 1)})
+"
+let breConn_rhs_to_parse =
+"
+{(0, UAV:1),(1, UAV:1)}
+2 2 0
+10
+01
+00
+00
+({}, {}, {(1, 1)})
+({}, {c1}, {(0, 1)})
+"
+
 let s0 = Big.of_string s0_to_parse
 let mov_lhs = Big.of_string move_lhs_to_parse
 let mov_rhs = Big.of_string move_rhs_to_parse
@@ -165,14 +187,17 @@ let estConn1AF_lhs = Big.of_string estConn1AF_lhs_to_parse
 let estConn1AF_rhs = Big.of_string estConn1AF_rhs_to_parse
 let estConn2AF_lhs = Big.of_string estConn2AF_lhs_to_parse
 let estConn2AF_rhs = Big.of_string estConn2AF_rhs_to_parse
+let breConn_lhs = Big.of_string breConn_lhs_to_parse
+let breConn_rhs = Big.of_string breConn_rhs_to_parse
 let mov_f_rnm = Fun.empty |> Fun.add 0 0 |> Fun.add 1 2 |> Fun.add 2 1
 let estConn2AF_f_rnm = Fun.empty |> Fun.add 0 0 |> Fun.add 1 1 |> Fun.add 2 2 |> Fun.add 3 3
 let estConn1AF_f_rnm = Fun.empty |> Fun.add 0 0 |> Fun.add 1 1 |> Fun.add 2 2
-
+let breConn_f_rnm = Fun.empty |> Fun.add 0 0 |> Fun.add 1 1
 let mov_react = TBrs.parse_react "move" ~lhs:mov_lhs ~rhs:mov_rhs ~f_sm:None ~f_rnm:mov_f_rnm
 let estConn1AF_react = TBrs.parse_react "estConn1AF" ~lhs:estConn1AF_lhs ~rhs:estConn1AF_rhs ~f_sm:None ~f_rnm:estConn1AF_f_rnm
 let estConn2AF_react = TBrs.parse_react "estConn2AF" ~lhs:estConn2AF_lhs ~rhs:estConn2AF_rhs ~f_sm:None ~f_rnm:estConn2AF_f_rnm
-let rules = [mov_react;estConn1AF_react;estConn2AF_react];;
+let breConn_react = TBrs.parse_react "breConn" ~lhs:breConn_lhs ~rhs:breConn_rhs ~f_sm:None ~f_rnm:breConn_f_rnm 
+let rules = [mov_react;estConn1AF_react;estConn2AF_react;breConn_react];;
 Parmap.set_default_ncores 4
 
 let tl,ss,uss,ms = TBrs.parexplore_ss ~s0 ~rules ~max_steps:300;;
