06d863941fbd4369901bbbee4904ea49888a00dc
zajer <pikus3@list.pl>
2020-04-03 10:13:31 +0200

    Instrumentation of the code.

--- prof/trs_prof.ml
+++ prof/trs_prof.ml
@@ -20,11 +20,11 @@ let is_site_mapping_function_correct f_sm ~(lhs:Big.t) ~(rhs:Big.t) =
     and is_fsm_to_not_exceeding = IntSet.max_elt (Fun.codom f_sm) < Some (lhs.p.s)
     in
         is_fsm_total && is_fsm_to_not_exceeding
-let is_residual_node_mapping_function_correct f_rnm ~(lhs:Big.t) ~(rhs:Big.t) =
+let [@landmark] is_residual_node_mapping_function_correct f_rnm ~(lhs:Big.t) ~(rhs:Big.t) =
     let is_frnm_not_exceeding = IntSet.max_elt (Fun.dom f_rnm) <= Some rhs.p.n && IntSet.max_elt (Fun.codom f_rnm) <= Some lhs.p.n
     in
         is_frnm_not_exceeding
-let parse_react l ~(lhs:Big.t) ~(rhs:Big.t) ~(f_sm:Fun.t option) ~(f_rnm:Fun.t) =
+let [@landmark] parse_react l ~(lhs:Big.t) ~(rhs:Big.t) ~(f_sm:Fun.t option) ~(f_rnm:Fun.t) =
     let is_f_sm_correct = match f_sm with 
         | None -> rhs.p.s = lhs.p.s
         | Some fsm -> is_site_mapping_function_correct fsm ~lhs ~rhs
@@ -36,16 +36,16 @@ let parse_react l ~(lhs:Big.t) ~(rhs:Big.t) ~(f_sm:Fun.t option) ~(f_rnm:Fun.t) 
         | _, false, _ -> raise (invalid_arg "Residual nodes mapping is not correct")
         | _, _, false -> raise (invalid_arg "Label cannot be empty")
         | true, true, true -> {label=l ; lhs ; rhs ; f_sm; f_rnm}
-let apply_trr_with_occ (b:Big.t) (r:react) (lhs_occ:Big.occ) =
+let [@landmark] apply_trr_with_occ (b:Big.t) (r:react) (lhs_occ:Big.occ) =
     let res_b,res_f = TBig.rewrite lhs_occ ~target:b ~r0:r.lhs ~r1:r.rhs ~f_s:r.f_sm ~f_r1_r0:r.f_rnm
     and res_iso = match lhs_occ with | iso, _, _ -> iso
     in
         { is=b; rs=res_b;rf=res_f;p=res_iso; rl=r.label}
-let apply_trr (b:Big.t) (r:react) =
+let [@landmark] apply_trr (b:Big.t) (r:react) =
     let occs = Big.occurrences ~target:b ~pattern:r.lhs
     in  
         List.fold_left (fun res occ -> apply_trr_with_occ b r occ :: res) [] occs
-let translate_trans ~(output_res_state:Big.t) ~(translated:t) =
+let [@landmark] translate_trans ~(output_res_state:Big.t) ~(translated:t) =
     let translation = TBig.translate_equal ~from_b:translated.rs ~to_b:output_res_state
     in
         let res_residue_fun = Utils.transform_fun_dom translated.rf translation
@@ -57,7 +57,7 @@ let translate_trans ~(output_res_state:Big.t) ~(translated:t) =
                 p=translated.p;
                 rl=translated.rl
             }      
-let split_into_iso_trans (patt:Big.t) (rest:t list) =
+let [@landmark] split_into_iso_trans (patt:Big.t) (rest:t list) =
     let patt_key = Big.key patt
     in
         List.fold_left 
@@ -72,13 +72,13 @@ let split_into_iso_trans (patt:Big.t) (rest:t list) =
             rest
 let translate_all ors ttl =
     List.fold_left (fun res t -> translate_trans ~translated:t ~output_res_state:ors::res) [] ttl
-let translate_all_iso_trans (patt:Big.t) (all:t list) =
+let [@landmark] translate_all_iso_trans (patt:Big.t) (all:t list) =
     let eq,neq = split_into_iso_trans patt all    
     in
         let teq = translate_all patt eq
         in
             teq,neq
-let rec unify_based_on_iso_res_states lot = 
+let [@landmark] rec unify_based_on_iso_res_states lot = 
     match lot with
     | [] -> []
     | t::rest -> 
@@ -87,7 +87,7 @@ let rec unify_based_on_iso_res_states lot =
             let merged_with_rest = unify_based_on_iso_res_states rest'
             in 
                 [t.rs,t::merged_with_t] @ merged_with_rest
-let rec group_based_on_iso_res_states lot = 
+let [@landmark] rec group_based_on_iso_res_states lot = 
     match lot with
         | [] -> []
         | t::rest -> 
@@ -96,14 +96,14 @@ let rec group_based_on_iso_res_states lot =
             let grouped_rest = group_based_on_iso_res_states rest'
             in 
                 [t.rs,t::equal_with_t] @ grouped_rest
-let step_grouped_iso_res b lr =
+let [@landmark] step_grouped_iso_res b lr =
     let raw_result = List.fold_left (fun res r -> apply_trr b r @ res) [] lr
         in  
             let grouped_result = group_based_on_iso_res_states raw_result
             in
                 grouped_result
 
-let find_iso_indexed_big (patt:Big.t) (loib:(Big.t*int) list) =
+let [@landmark] find_iso_indexed_big (patt:Big.t) (loib:(Big.t*int) list) =
     let patt_key = Big.key patt
     in
         List.fold_left 
@@ -118,7 +118,7 @@ let find_iso_indexed_big (patt:Big.t) (loib:(Big.t*int) list) =
             loib
      
 (* reindex_from contains less elements than reindex_of  *)
-let filter_and_reindex_duplicates_case1 ~reindex_of:(rof:(Big.t * int) list ) ~reindex_from:(rfr:(Big.t * int) list ) =
+let [@landmark] filter_and_reindex_duplicates_case1 ~reindex_of:(rof:(Big.t * int) list ) ~reindex_from:(rfr:(Big.t * int) list ) =
     List.fold_left 
     (
         fun (rest_unique,rest_isos) (b_rfr,rfr_idx) -> 
@@ -132,7 +132,7 @@ let filter_and_reindex_duplicates_case1 ~reindex_of:(rof:(Big.t * int) list ) ~r
     ([],[])
     rfr
 (* reindex_of contains less elements than reindex_from  *)
-let filter_and_reindex_duplicates_case2 ~reindex_of:(rof:(Big.t * int) list ) ~reindex_from:(rfr:(Big.t * int) list ) =
+let [@landmark] filter_and_reindex_duplicates_case2 ~reindex_of:(rof:(Big.t * int) list ) ~reindex_from:(rfr:(Big.t * int) list ) =
     let isos, unique = List.fold_left 
     (
         fun (rest_isos,rest_from) (b_rof,rof_idx) -> 
@@ -147,20 +147,20 @@ let filter_and_reindex_duplicates_case2 ~reindex_of:(rof:(Big.t * int) list ) ~r
     rof
     in
         unique,isos
-let filter_and_reindex_duplicatesV2 ~reindex_of:(rof:(Big.t * int) list ) ~reindex_from:(rfr:(Big.t * int) list ) =
+let [@landmark] filter_and_reindex_duplicatesV2 ~reindex_of:(rof:(Big.t * int) list ) ~reindex_from:(rfr:(Big.t * int) list ) =
     let rfr_count = List.length rfr
     and rof_count = List.length rof
     in
         if rfr_count >= rof_count then filter_and_reindex_duplicates_case2 ~reindex_of:rof ~reindex_from:rfr
         else filter_and_reindex_duplicates_case1 ~reindex_of:rof ~reindex_from:rfr
      
-let regen_indexing (ci:int) (ri:(Big.t * int) list) =
+let [@landmark] regen_indexing (ci:int) (ri:(Big.t * int) list) =
     let indexing = List.mapi (fun i (b,_) -> b,i+ci) ri
     in
         let iso = List.map2 (fun (_,oi) (_,ri) -> (oi,ri)) ri indexing
         in
             indexing,iso
-let apply_reindexing loit ridx =
+let [@landmark] apply_reindexing loit ridx =
     List.map
         (
             fun (t,init_idx,res_idx) ->
@@ -171,7 +171,7 @@ let apply_reindexing loit ridx =
                         | Some (_, res_idx') -> t,init_idx,res_idx'
         )
         loit
-let apply_reindexing_exclude_rest loit ridx =
+let [@landmark] apply_reindexing_exclude_rest loit ridx =
         List.fold_right
             (
                 fun (t,init_idx,res_idx) (res_app,res_exc) ->
@@ -183,7 +183,7 @@ let apply_reindexing_exclude_rest loit ridx =
             )
             loit
             ([],[])        
-let initial_indexing (btll:(Big.t * t list) list ) ~(init_state_idx:int) ~(checked_unchecked_sum:int) =
+let [@landmark] initial_indexing (btll:(Big.t * t list) list ) ~(init_state_idx:int) ~(checked_unchecked_sum:int) =
         let mapped = List.mapi
             (
                 fun res_state_idx_no_shift (b,tl) ->
@@ -205,7 +205,7 @@ let initial_indexing (btll:(Big.t * t list) list ) ~(init_state_idx:int) ~(check
         ([],[])
         
             
-let _gen_trans_and_unique_states 
+let [@landmark] _gen_trans_and_unique_states 
     ~(rules:react list) 
     ~(checked:(Big.t*int) list) 
     ~unchecked 
@@ -236,7 +236,7 @@ let _gen_trans_and_unique_states
                                 my_trans@trans, 
                                 my_new_unchecked_states_reindexed@new_unchecked_states,
                                 ( (List.length my_new_unchecked_states_reindexed)+new_unchecked_states_number )
-let _pargen_of_trans_and_unique_states ~(rules:react list) ~(checked:(Big.t * int) list) ~unchecked =
+let [@landmark] _pargen_of_trans_and_unique_states ~(rules:react list) ~(checked:(Big.t * int) list) ~unchecked =
     let checked_unchecked_sum = List.length checked + List.length unchecked
     in
     List.fold_right
@@ -442,13 +442,13 @@ let mov_f_rnm = Fun.empty |> Fun.add 0 0 |> Fun.add 1 2 |> Fun.add 2 1
 let estConn2AF_f_rnm = Fun.empty |> Fun.add 0 0 |> Fun.add 1 1 |> Fun.add 2 2 |> Fun.add 3 3
 let estConn1AF_f_rnm = Fun.empty |> Fun.add 0 0 |> Fun.add 1 1 |> Fun.add 2 2
 
-let mov_react = TBrs.parse_react "move" ~lhs:mov_lhs ~rhs:mov_rhs ~f_sm:None ~f_rnm:mov_f_rnm
-let estConn1AF_react = TBrs.parse_react "estConn1AF" ~lhs:estConn1AF_lhs ~rhs:estConn1AF_rhs ~f_sm:None ~f_rnm:estConn1AF_f_rnm
-let estConn2AF_react = TBrs.parse_react "estConn2AF" ~lhs:estConn2AF_lhs ~rhs:estConn2AF_rhs ~f_sm:None ~f_rnm:estConn2AF_f_rnm
+let mov_react = parse_react "move" ~lhs:mov_lhs ~rhs:mov_rhs ~f_sm:None ~f_rnm:mov_f_rnm
+let estConn1AF_react = parse_react "estConn1AF" ~lhs:estConn1AF_lhs ~rhs:estConn1AF_rhs ~f_sm:None ~f_rnm:estConn1AF_f_rnm
+let estConn2AF_react = parse_react "estConn2AF" ~lhs:estConn2AF_lhs ~rhs:estConn2AF_rhs ~f_sm:None ~f_rnm:estConn2AF_f_rnm
 let rules = [mov_react;estConn1AF_react;estConn2AF_react];;
-Parmap.set_default_ncores 4
 
-let tl,ss,uss,ms = TBrs.parexplore_ss ~s0 ~rules ~max_steps:300;;
-              
+Landmark.start_profiling ();;
+let tl,ss,uss,ms = parexplore_ss ~s0 ~rules ~max_steps:300;;
+
 print_endline ("Number of transitions:" ^ ( string_of_int (List.length tl) ) );
 print_endline ("Number of unique states:" ^ ( string_of_int (List.length ss) ) );;
